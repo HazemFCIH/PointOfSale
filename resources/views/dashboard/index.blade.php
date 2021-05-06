5c152d9a94be99ce4150a3639b7f93403223441c
HazemFCIH <hazem.fcih12@gmail.com>
2021-05-06 16:18:37 +0200

    initial commit

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.dashboard.app')
+@section('content')
+<div class="content-wrapper">
+
+    <section class="content-header">
+        <h1>Blank page <small>it all starts here</small></h1>
+        <ol class="breadcrumb"...>
+    </section>
+    <section class="content">
+<h1>this is dashboard</h1>
+    </section>
+
+</div>
+@endsection
